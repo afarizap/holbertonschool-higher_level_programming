3fa045332643aa355cf6ed52e256607226161e69
afarizap <afarizap@gmail.com>
2020-04-28 19:51:21 +0000

    fix negative numbers and 1 digit numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if 10 > number > -10:
-    lastd = number
+numberabs = abs(number)
+if numberabs < 10:
+    lastdigit = numberabs
 else:
-    lastd = number % 10
-if lastd > 5:
-    n = "greater than 5"
-    print("Last digit of {:n} is {:n} and is {:s}".format(number, lastd, n))
-elif lastd == 0:
-    print("Last digit of {:n} is {:n} and is 0".format(number, lastd))
-elif lastd < 6:
-    n = "less than 6 and not 0"
-    print("Last digit of {:n} is {:n} and is {:s}".format(number, lastd, n))
+    lastdigit = numberabs % 10
+
+print("Last digit of {} is {} and is ".format(number, lastdigit), end = "")
+if lastdigit > 5:
+    print("greater than 5")
+elif lastdigit == 0:
+    print("0")
+elif lastdigit < 6:
+    print("less than 6 and not 0")
