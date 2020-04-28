b1921c7a650ee0e40492c350d323ff36b5c7611a
afarizap <afarizap@gmail.com>
2020-04-28 20:32:03 +0000

    fix negative numbers and 1 digit numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,12 @@ if numberabs < 10:
     lastdigit = numberabs
 else:
     lastdigit = numberabs % 10
-
-print("Last digit of {} is {} and is ".format(number, lastdigit), end = "")
+if number < 0:
+    lastdigit = lastdigit * -1
+print("Last digit of {} is {} and is ".format(number, lastdigit), end="")
 if lastdigit > 5:
     print("greater than 5")
 elif lastdigit == 0:
     print("0")
-elif lastdigit < 6:
+else:
     print("less than 6 and not 0")
