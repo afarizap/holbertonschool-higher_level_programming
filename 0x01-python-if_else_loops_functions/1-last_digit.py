75ab2b70318c8f1b866b325e946194f99add88d8
afarizap <afarizap@gmail.com>
2020-04-28 19:31:30 +0000

    fix numbers between 10 and -10

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastd = number % 10
+if 10 > number > -10:
+    lastd = number
+else:
+    lastd = number % 10
 if lastd > 5:
     n = "greater than 5"
     print("Last digit of {:n} is {:n} and is {:s}".format(number, lastd, n))
