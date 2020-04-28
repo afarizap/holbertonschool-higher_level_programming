b71a889de39294e76ad2a12576bd9bfba6e673f2
afarizap <afarizap@gmail.com>
2020-04-28 03:28:59 +0000

    add cycle checker

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle = list;
+	listint_t *hare = list;
+
+	turtle = turtle->next;
+	hare = hare->next;
+	hare = hare->next;
+
+	while (turtle && hare)
+	{
+		if (turtle == hare)
+			return (1);
+		turtle = turtle->next;
+		hare = hare->next;
+		hare = hare->next;
+	}
+	return (0);
+}
