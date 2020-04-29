108beee00a2853a8175d17186ccd6f9a09eba2ce
afarizap <afarizap@gmail.com>
2020-04-29 00:45:07 +0000

    add len, chr, ord

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        letter = ord(str[i])
+        if letter >= 97:
+            if letter <= 122:
+                letter -= 32
+        print("{}".format(chr(letter)), end="")
+    print()
