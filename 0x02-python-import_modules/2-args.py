d905c1dda07798d5ffa613c12a819b4f5caa6e2f
afarizap <afarizap@gmail.com>
2020-04-30 19:49:11 +0000

    fixed .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,10 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 1:
         print("{:d} argument:".format(argc))
-    else:
+    elif argc == 0:
         print("{:d} arguments.".format(argc))
+    else:
+        print("{:d} arguments:".format(argc))
     x = 0
     for i in range(argc):
         print("{:d}: {:s}".format(i + 1, str(sys.argv[i + 1])))
