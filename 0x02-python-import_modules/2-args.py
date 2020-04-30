63463304840af81557a180408932da8f6e6ff636
afarizap <afarizap@gmail.com>
2020-04-30 19:43:17 +0000

    fixed format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("0 arguments.")
+    argc = len(sys.argv) - 1
+    if argc == 1:
+        print("{:d} argument:".format(argc))
     else:
-        if len(sys.argv) == 2:
-            print("1 argument:")
-            print("1: {:s}".format(str(sys.argv[1])))
-        else:
-            print("{:d} arguments:".format(len(sys.argv)))
-            for i in range(len(sys.argv)):
-                print("{:d}: {:s}".format(i, str(sys.argv[i])))
+        print("{:d} arguments.".format(argc))
+    x = 0
+    for i in range(argc):
+        print("{:d}: {:s}".format(i + 1, str(sys.argv[i + 1])))
