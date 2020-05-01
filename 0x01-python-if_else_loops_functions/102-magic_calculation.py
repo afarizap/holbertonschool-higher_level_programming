3a6552478d08a620560ac44c9f26336ee73be8fe
afarizap <afarizap@gmail.com>
2020-05-01 23:04:19 +0000

    fix pep8 assembly pop jump as if  and compare op <

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b, c):
     if a < b:
         return c
     if c > b:
-       return a + b
+        return a + b
     return a * b - c
 if __name__ == '__main__':
     import dis
