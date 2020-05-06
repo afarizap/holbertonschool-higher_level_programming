9c511d6bb68fee0a91ec0290d19892936934d8c1
afarizap <afarizap@gmail.com>
2020-05-06 01:41:28 +0000

    rly

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    R = len(matrix)
-    for i in range(R):
-        print(*matrix[i])
+    print('\n'.join([' '.join(
+        ['{:d}'.format(idx)for idx in row]
+    ) for row in matrix]))
