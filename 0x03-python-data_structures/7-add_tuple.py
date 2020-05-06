b662049f97db708c99c05b87ad6f4d47611baa7e
afarizap <afarizap@gmail.com>
2020-05-06 02:41:55 +0000

    fix bigger than 2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 2:
-        pass
+    if len(tuple_a) <= 2:
+        tuple_a = tuple_a[0], tuple_a[1]
     else:
         if len(tuple_a) == 1:
             if tuple_a[0]:
@@ -10,8 +10,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 tuple_a = 0, tuple_a[1]
         else:
             tuple_a = 0, 0
-    if len(tuple_b) == 2:
-        pass
+    if len(tuple_b) <= 2:
+        tuple_b = tuple_b[0], tuple_b[1]
     else:
         if len(tuple_b) == 1:
             if tuple_b[0]:
