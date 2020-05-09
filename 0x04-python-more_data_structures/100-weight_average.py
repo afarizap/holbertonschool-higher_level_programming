8f04e984e936d9d8d8d8847b13687dc64faa0cf7
afarizap <afarizap@gmail.com>
2020-05-09 23:50:06 +0000

    add weight avrg

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum2 = 0
+    quot = 0
+    for i in my_list:
+        mul1 = 1
+        quot += i[1]
+        for j in i:
+            mul1 *= j
+        sum2 += mul1
+    res = sum2 / quot
+    return res
