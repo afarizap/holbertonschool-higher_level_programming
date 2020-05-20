f46f2d41631c331694bc6138acdca1624ce88b0f
afarizap <afarizap@gmail.com>
2020-05-20 03:53:20 +0000

    add init, self, private class method/attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" this is the script"""
+
+
+class Square:
+     """ size as private class atribute/method, initialized and defined"""
+     def __init__(self, size):
+          self.__size = size
