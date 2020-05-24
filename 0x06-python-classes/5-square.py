15f56701522e72d62ec97a4ff9689693eb403c94
afarizap <afarizap@gmail.com>
2020-05-24 23:08:20 +0000

    add square made with #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" area of square """
+
+
+class Square:
+    """ Definition of square area """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(0, self.size):
+                for j in range(0, self.size):
+                    print("#", end="")
+                print()
