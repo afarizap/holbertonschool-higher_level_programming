f356b19b257bb3c6121baf0b74ad2bab2f82fa20
afarizap <afarizap@gmail.com>
2020-05-27 13:59:04 +0000

    magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def magic_string(x):
-    x += "Holberton"
-    return n * magic_string.x
-if __name__ == "__main__":
-    import dis
-    dis.dis(magic_string)
+def magic_string(x=[]):
+    x += ["Holberton"]
+    return ", ".join(map(str, x))
