6da422494c3809e57fc20ffe1a1e48ea92811963
afarizap <afarizap@gmail.com>
2020-05-27 13:40:24 +0000

    fix 19 add some others

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def magic_string(x):
+    x += "Holberton"
+    return n * magic_string.x
+if __name__ == "__main__":
+    import dis
+    dis.dis(magic_string)
--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-""" This copy a list"""
-
-
 def copy_list(l):
-    """ create a new list and return"""
-    new = l[:]
-    return new
+    return list(l[:])
