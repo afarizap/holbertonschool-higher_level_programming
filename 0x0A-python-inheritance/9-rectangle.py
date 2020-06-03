366f9d3e1373e2abb01895fe340430f971558686
afarizap <afarizap@gmail.com>
2020-06-03 04:48:55 +0000

    rest of mandatory

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" empty class """
+
+
+class BaseGeometry:
+    """ Base Geometry Class """
+    def area(self):
+        """ exception """
+        raise Exception("area() is not implemented Exception")
+
+    def integer_validator(self, name, value):
+        """ validate if value is an in """
+        if type(value) is not int:
+            raise TypeError ("<name> must be an integer")
+        if value <= 0:
+            raise ValueError ("<name> must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ Rectangle Class """
+
+
+    def __init__(self, width, height):
+        """ validate and private values """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ area of rectangle """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ print legible rectangle fields """
+        a = "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return a
