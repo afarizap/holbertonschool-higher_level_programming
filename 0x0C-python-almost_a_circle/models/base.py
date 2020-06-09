20faafc9046354c2bafaf9771c308bc3780ec3c1
afarizap <afarizap@gmail.com>
2020-06-09 02:16:39 +0000

    add up to 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,13 +16,24 @@ class Base:
             self.id = self.__nb_objects
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or len(list_dictionaries):
-            listd = {}
-            return listd
+        if list_dictionaries is None or len(list_dictionaries) is 0:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        with open(cls.__name__ + ".json", "a", encoding="utf-8") as f:
-            f.write(Base.to_json_string(list_objs))
-            print(list_objs)
+        jlist = []
+        if len(list_objs) is 0:
+            pass
+        else:
+            for obj in list_objs:
+                jlist.append(obj.to_dictionary())
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(jlist))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) is 0:
+            return []
+        else:
+            return json.loads(json_string)
