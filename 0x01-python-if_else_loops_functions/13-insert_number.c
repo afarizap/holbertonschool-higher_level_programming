4eee60ac2d91289984325b4d9bca429b8b59419e
afarizap <afarizap@gmail.com>
2020-09-02 04:07:52 +0000

    if no head

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,9 +13,12 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *aux = *head, *new;
 
+	if (!head)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
+
 	new->n = number;
 	new->next = NULL;
 
