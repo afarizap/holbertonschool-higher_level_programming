ff4bb470dfba7fade6800765c628c17e3e1de3b7
afarizap <afarizap@gmail.com>
2020-09-09 04:58:41 +0000

    structure

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: no idea yet :v
+ */
+void print_python_list_info(PyObject *p)
+{
+	printf("[*] Size of the Python List = %d", len);
+	printf("[*] Allocated = %d", len);
+	printf("Element %d: %s", i, type);
+}
