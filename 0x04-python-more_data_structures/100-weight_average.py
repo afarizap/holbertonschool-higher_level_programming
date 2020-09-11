ace1ad9ea13d7f3ffa89ada284ae2058814afa46
afarizap <afarizap@gmail.com>
2020-09-11 03:28:44 +0000

    add validation empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,13 +2,13 @@
 def weight_average(my_list=[]):
     sum2 = 0
     quot = 0
-    if not list:
-        return 0
     for i in my_list:
         mul1 = 1
         quot += i[1]
         for j in i:
             mul1 *= j
         sum2 += mul1
+    if not list or quot == 0:
+        return 0
     res = sum2 / quot
     return res
