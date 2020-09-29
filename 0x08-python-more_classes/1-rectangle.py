f6c7178f26c971b0706a852fc8f3dbca6e2fad70
afarizap <afarizap@gmail.com>
2020-09-29 01:24:28 +0000

    testing decorators and fix sybtax

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,11 +11,9 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    @property
     def width(self):
         return self.__width
 
-    @width.setter
     def width(self, width):
         if type(width) is not int:
             raise TypeError("width must be an integer")
@@ -23,11 +21,9 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = width
 
-    @property
     def height(self):
         return self.__height
 
-    @height.setter
     def height(self, height):
         if type(height) is not int:
             raise TypeError("height must be an integer")
