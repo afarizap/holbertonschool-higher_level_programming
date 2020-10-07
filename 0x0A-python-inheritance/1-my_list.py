e995f2a0457def298c0e3b19d6498cd7187eb9b7
afarizap <afarizap@gmail.com>
2020-10-07 01:56:28 +0000

    fix pep8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,11 @@
 class MyList(list):
     """ class MyList """
 
-
     def print_sorted(self):
         """ print the list """
         print(sorted(self))
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("./tests/1-my_list.txt")
