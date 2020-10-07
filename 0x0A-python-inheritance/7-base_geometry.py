feca1524e5ad5e53eaa2de388beee9813005ffdd
afarizap <afarizap@gmail.com>
2020-10-07 14:27:16 +0000

    fixa lot

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,10 +5,10 @@
 class BaseGeometry:
     """ Base Geometry Class """
     def area(self):
-        raise Exception("area() is not implemented Exception")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if type(value) is not int:
-            raise TypeError ("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError ("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
