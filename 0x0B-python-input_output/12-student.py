37cf253a9430f6bd94cbc2e75db3eb666e14446b
afarizap <afarizap@gmail.com>
2020-10-08 02:25:15 +0000

    c:

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Student to JSON """
+
+
+class Student:
+    """ defines a student by """
+
+    def __init__(self, first_name, last_name, age):
+        """ instantiation public """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves JSON representation of a student instance """
+        new = self.__dict__.copy()
+        if attrs and isinstance(attrs, list):
+            for i in self.__dict__.keys():
+                if i in attrs:
+                    continue
+                else:
+                    new.pop(i)
+        return new
