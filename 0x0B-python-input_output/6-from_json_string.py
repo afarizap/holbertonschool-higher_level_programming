505df446c859edb54accab248216b0a2ccc49c4e
afarizap <afarizap@gmail.com>
2020-10-08 00:26:07 +0000

    change import positino

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ From JSON string to Object """
+import json
 
 
 def from_json_string(my_str):
     """ return an object (python) represented by json string """
-    import json
     return json.loads(my_str)
