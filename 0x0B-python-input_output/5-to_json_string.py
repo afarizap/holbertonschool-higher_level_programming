e69dfb747af74f90232002926849ca6c311226d4
afarizap <afarizap@gmail.com>
2020-10-08 00:23:38 +0000

    move import position

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ To JSON string """
+import json
 
 
 def to_json_string(my_obj):
     """ returns the JSON representaiton of an object (sstring) """
-    import json
     return json.dumps(my_obj)
