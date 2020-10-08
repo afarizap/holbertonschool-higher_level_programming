ef3938f13dc5f2bce6715a863a93904be3900399
afarizap <afarizap@gmail.com>
2020-10-08 01:04:24 +0000

    fix comments

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,11 +6,11 @@ class Student:
     """ defines a student by """
 
     def __init__(self, first_name, last_name, age):
-        """ instantiation priv """
+        """ instantiation public """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ retrieves dictionary representation of a student instance """
+        """ retrieves JSON representation of a student instance """
         return self.__dict__
