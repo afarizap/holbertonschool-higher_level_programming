077093b145865921409f65788a75fbcf736b7094
afarizap <afarizap@gmail.com>
2020-10-29 19:38:54 +0000

    fix err

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,6 +5,8 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """ here start unit test functions """
+
     def test_empty(self):
         """ test max integer when empty list given"""
         self.assertEqual(max_integer([]), None)
@@ -12,4 +14,10 @@ class TestMaxInteger(unittest.TestCase):
     def test_is_int(self):
         """ test if the given arg is an int """
         self.assertIs(type(max_integer([1])), int)
-        self.assertIs(type(max_integer([a])), int)
+
+    def max_at_the_end(self):
+        """ test if there is a max in the end of the list"""
+        pass
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
