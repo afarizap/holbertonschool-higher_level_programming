e6738a8543f2d58636d71853d67efe1d7e141f0b
afarizap <afarizap@gmail.com>
2020-11-17 16:38:34 +0000

    source: https://stackoverflow.com/questions/5555328/error-1396-hy000-operation-create-user-failed-for-jacklocalhost

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- creates the MySQL server user user_0d_1.
+DROP USER 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER 'user_0d_1'@'localhost'
+       IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+ON *.*
+TO 'user_0d_1'@'localhost';
