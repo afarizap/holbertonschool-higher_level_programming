f5cdc5f9e3d7eeeb650b3aab25b8c02b9d8df5ab
afarizap <afarizap@gmail.com>
2020-11-18 05:04:45 +0000

    fix record title display

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT DISTINCT tv_shows.title title, tv_genres.name name
+SELECT DISTINCT tv_shows.title, tv_genres.name
 FROM tv_show_genres
 RIGHT OUTER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
