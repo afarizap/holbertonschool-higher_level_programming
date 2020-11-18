c2cb9f2e366dd7467ad05f17ab17aba4b27a92df
afarizap <afarizap@gmail.com>
2020-11-18 05:07:45 +0000

    fix ORDERED BY

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ RIGHT OUTER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 RIGHT OUTER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_genres.name;
+ORDER BY title, name;
