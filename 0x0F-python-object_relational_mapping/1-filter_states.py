7b69a3efe8b123763bd27ad458ad8708ce95babd
afarizap <afarizap@gmail.com>
2020-12-11 23:56:08 +0000

    some fixes and tsk1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" lists all states from the database hbtn_0e_0_usa """
+""" lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa """
 
 
 if __name__ == "__main__":
@@ -18,4 +19,5 @@ if __name__ == "__main__":
     r = cursor.fetchall()
 
     for i in r:
-        print(i)
+        if i[1][0] == 'N':
+            print(i)
