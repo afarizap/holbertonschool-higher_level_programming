5b14fa9aeda7c9c07ea4e851e867a17e9658aa11
afarizap <afarizap@gmail.com>
2020-12-13 14:37:59 +0000

    add task2, new argument to evaluate at the end (name)

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in
+ the states table of hbtn_0e_0_usa where name matches the argument """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pss = sys.argv[2]
+    dbn = sys.argv[3]
+    stn = sys.argv[4]
+
+    dbc = MySQLdb.connect(user=usr, passwd=pss, db=dbn)
+
+    cursor = dbc.cursor()
+    cursor.execute("SELECT * FROM states")
+
+    r = cursor.fetchall()
+
+    for i in r:
+        if i[1] == stn:
+            print(i)
