9468aae5c88f5023b882c0f3a7ecdd3ba4a7e945
afarizap <afarizap@gmail.com>
2020-12-13 14:17:53 +0000

    comments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-""" lists all states from the database hbtn_0e_0_usa """
-
+""" lists all states from the database hbtn_0e_0_usa, importa solo cuando se
+ llama el main los argumentos y el modulo MySQL, Se asignan los valores
+a buscar y se conecta la base de datos, se inicializa el cursor y se
+ejecuta el query deseado, se devuelven todos los resultados dentro de la
+variable r, Se itera en r cada linea para imprimir"""
 
 if __name__ == "__main__":
-    """ importa solo cuando se llama el main los argumentos y el modulo MySQL"""
     import sys
     import MySQLdb
 
-    """ Se asignan los valores a buscar y se conecta la base de datos"""
     usr = sys.argv[1]
     pss = sys.argv[2]
     dbn = sys.argv[3]
 
     dbc = MySQLdb.connect(user=usr, passwd=pss, db=dbn)
 
-    """ se inicializa el cursor y se ejecuta el query deseado """
     cursor = dbc.cursor()
     cursor.execute("SELECT * FROM states")
 
-    """ se devuelven todos los resultados dentro de la variable r """
     r = cursor.fetchall()
 
-    """ Se itera en r cada linea para imprimir"""
     for i in r:
         print(i)
