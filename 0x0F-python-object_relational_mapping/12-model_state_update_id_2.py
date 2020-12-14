b779c44517d99c29ded1e9d387768fa4b577744a
afarizap <afarizap@gmail.com>
2020-12-14 03:48:37 +0000

    filter obj and change ob attribute

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """ create all the data based on .sql file engine == Source ,
+    metadata.create_all() is like exec"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    """ we start talking to de db, with the orm module we define Session class
+    which will serve as a factory for new session objects"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """ querying """
+    modify_ob = session.query(State).filter_by(id=2).first()
+    modify_ob.name = "New Mexico"
+    session.commit()
