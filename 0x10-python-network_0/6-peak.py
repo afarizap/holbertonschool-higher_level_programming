80e9d2d43c4f211aecf144cc0eb76b01c1ac8aa4
afarizap <afarizap@gmail.com>
2020-12-19 00:05:06 +0000

    use of builtin

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,16 +3,6 @@
 
 
 def find_peak(list_of_integers):
-    l = list_of_integers
-    if not l:
-        return None
-    if len(l) == 1:
-        return l[0]
-    left = l[0]
-    for i in l:
-        right = i
-        if left > right:
-            return left
-        if left < right:
-            left = right
-    return left
+    if list_of_integers:
+        return max(list_of_integers)
+    return None
