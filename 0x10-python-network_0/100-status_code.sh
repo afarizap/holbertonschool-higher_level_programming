3e76dee3e5f59bba66bfce0a590c0a570ae9598e
afarizap <afarizap@gmail.com>
2020-12-19 00:50:14 +0000

    https://medium.com/@kaneshin0120/getting-http-status-code-by-curl-4a8cf908a544

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL, and displays only the status code of the response
-curl -sI -o /dev/null -w "%{http_code}" $1
+curl --silent --include --output /dev/null --write-out "%{http_code}" $1
