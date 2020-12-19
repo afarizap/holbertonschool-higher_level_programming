80e47aa6118dc0e60e600b493c48af727cc77305
afarizap <afarizap@gmail.com>
2020-12-19 00:22:45 +0000

    https://www.ics.uci.edu/~pattis/ICS-33/lectures/complexitypython.txt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def find_peak(list_of_integers):
+    """ actually sort the integers to find the max value """
     if list_of_integers:
         list_of_integers.sort()
         return list_of_integers[-1]
