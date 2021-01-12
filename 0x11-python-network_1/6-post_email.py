7e8967217c0b5dcd1f2541be737b7f550fed02b7
afarizap <afarizap@gmail.com>
2021-01-12 04:53:46 +0000

    fix imports

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,10 +2,10 @@
 """ Takes in a URL and an email address, sends a POST request with the email
  as a parameter, and finally displays the body.
 """
-from request import post
-from sys import argv
+import requests
+import sys
 
 
 if __name__ == '__main__':
-    r = post(argv[1], {'email': argv[2]})
+    r = requests.post(sys.argv[1], {'email': sys.argv[2]})
     print(r.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ takes in a URL, sends a request and displays the body
 """
-from request import get
-from sys import argv
+import requests
+import sys
 
 
 if __name__ == '__main__':
-    r = get(argv[1], {'email': argv[2]})
+    r = requests.get(sys.argv[1])
     if r.code >= 400:
         print('Error code: {}'.format(r.code))
     else:
