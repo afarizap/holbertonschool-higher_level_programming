b1ccef36bfb278f2200798ae0501a0cb4aeab2ae
afarizap <afarizap@gmail.com>
2021-01-12 04:56:45 +0000

    fix module call code for status_code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == '__main__':
     r = requests.get(sys.argv[1])
-    if r.code >= 400:
-        print('Error code: {}'.format(r.code))
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
     else:
         print(r.text)
