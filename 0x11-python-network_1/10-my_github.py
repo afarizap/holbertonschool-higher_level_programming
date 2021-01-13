536f79c4d970f3923bcf6f7bc3347344968dbca0
afarizap <afarizap@gmail.com>
2021-01-13 21:13:00 +0000

    pep8 fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,5 +7,6 @@ import sys
 
 
 if __name__ == '__main__':
-    r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
     print(r.json().get('id'))
