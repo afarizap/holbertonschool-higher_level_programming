cc752e5ff0cf70bb50d7c1efac3426cf535ac159
afarizap <afarizap@gmail.com>
2021-01-13 17:27:27 +0000

    fix json and argv

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,17 +8,15 @@ import sys
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
-    data = {'q': ''}
 
-    if sys.argv[1]:
-        data['q'] = sys.argv[1]
-
-    r = requests.post(url, data)
-    if r.get(data[q]):
-        try:
-            valid = json.loads(r)
-            print('[{}] {}'.format(r.get('id'), r.get('name')))
-        except:
-            print('Not a valid JSON')
+    if len(sys.argv) == 1:
+        q = ''
     else:
-        print('No result')
+        q = sys.argv[1]
+
+    r = requests.post(url, data={'q': q})
+
+    if r.json():
+        print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
+    else:
+        print("No result")
