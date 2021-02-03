978d6e827aa16b530394e9a16ca5b0d2521d2961
afarizap <afarizap@gmail.com>
2021-02-02 19:33:02 -0500

    fix task 2, there was another conditional when more than 1 arg is given, refactored.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
+if (process.argv[3]) {
+  console.log('Arguments found');
+} else if (process.argv[2]) {
   console.log('Argument found');
+} else {
+  console.log('No argument');
 }
