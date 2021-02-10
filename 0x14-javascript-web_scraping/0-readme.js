938395870f2e0e105be85ba49efa21af1e523e3c
afarizap <afarizap@gmail.com>
2021-02-09 20:56:20 -0500

    fix semistandard format in both files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
 try {
-  const data = fs.readFileSync(process.argv[2], 'utf8')
-  console.log(data)
+  const data = fs.readFileSync(process.argv[2], 'utf8');
+  console.log(data);
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
 try {
-  const data = fs.writeFileSync(process.argv[2], process.argv[3])
+  fs.writeFileSync(process.argv[2], process.argv[3]);
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }
