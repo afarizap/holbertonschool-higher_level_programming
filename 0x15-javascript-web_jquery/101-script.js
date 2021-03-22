2f89f8648cd7b545ad21b39cd3f76d4a880f4823
Andres Ariza <60362951+afarizap@users.noreply.github.com>
2021-03-22 15:45:49 -0500

    Update 101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,3 @@
-
+$('DIV#add_item').click(() => $('UL.my_list').append('<li>Item</li>'));
+$('DIV#remove_item').click(() => $('UL.my_list').children().last().remove());
+$('DIV#clear_list').click(() => $('UL.my_list').children().remove());
