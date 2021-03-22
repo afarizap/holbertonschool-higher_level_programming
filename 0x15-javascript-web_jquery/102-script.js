5b9fc07a8a2d1e26b6e7223d71c18174ccd3d7d1
Andres Ariza <60362951+afarizap@users.noreply.github.com>
2021-03-22 17:10:12 -0500

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,5 @@
-
+$('INPUT#btn_translate').click(() => {
+  const lang = $('INPUT#language_code').val();
+  $.get('https://www.fourtonfish.com/hellosalut/' + '?lang=' + lang,
+        (data) => $('DIV#hello').text(data.hello));
+});
