439f8c809ff0f346a6318b5500adfb8a27ed4af8
Andres Ariza <60362951+afarizap@users.noreply.github.com>
2021-03-22 17:49:12 -0500

    Update 103-script.js

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,12 @@
-
+function Holis () {
+  const lang = $('INPUT#language_code').val();
+  $.get('https://www.fourtonfish.com/hellosalut/' + '?lang=' + lang,
+        (data) => $('DIV#hello').text(data.hello));
+}
+$('INPUT#btn_translate').click(Holis);
+$('INPUT#language_code').keypress(function (event) {
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if(keycode === 13){
+        Holis;  
+    }
+}); 
